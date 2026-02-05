331db3fcb1261cba629edf0e77e184454be76465
leenus06 <146060227+leenus06@users.noreply.github.com>
2026-02-05 09:36:57 +0530

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -1,11 +1,5 @@
 variable "ssh_key_name" {
-  description = "The name of the SSH key pair to use for instances"
+  description = "SSH key pair name for EKS nodes"
   type        = string
   default     = "DevOps-Shack"
-}
-
-variable "instance_type" {
-  description = "EKS node instance type"
-  type        = string
-  default     = "m7i-flex.large"
 }
